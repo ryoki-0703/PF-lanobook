da9d9699b6032e367ad7d32291e307a0e71a8299
ryoki-0703 <tacchimi504@gmail.com>
2021-03-08 05:41:43 +0000

    いいね機能を非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,13 @@
 class FavoritesController < ApplicationController
   def create
-    lanobe = Lanobe.find(params[:lanobe_id])
-    favorite = current_user.favorites.new(lanobe_id: lanobe.id)
+    @lanobe = Lanobe.find(params[:lanobe_id])
+    favorite = current_user.favorites.new(lanobe_id: @lanobe.id)
     favorite.save
-    redirect_back(fallback_location: lanobe_path(lanobe))
   end
 
   def destroy
-    lanobe = Lanobe.find(params[:lanobe_id])
-    favorite = current_user.favorites.find_by(lanobe_id: lanobe.id)
+    @lanobe = Lanobe.find(params[:lanobe_id])
+    favorite = current_user.favorites.find_by(lanobe_id: @lanobe.id)
     favorite.destroy
-    redirect_back(fallback_location: lanobe_path(lanobe))
   end
 end
