7efd1716ff7087539745af3f5f1d51e3afe04fdd
ryoki-0703 <tacchimi504@gmail.com>
2021-03-13 10:46:40 +0000

    ヘッダー、フッターのレイアウト完成

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,14 +31,20 @@ $(function() {
     autoplaySpeed: 3000,
     speed: 1000,
     infinite: true,
+    pauseOnFocus: false,
     slidesToShow: 3,
     slidesToScroll: 1
   });
-  $("lanobe-image").hover(function(){
-       $(this).fadeTo("4000",0.3); // マウスオーバーで透明度を30%にする
+
+  $("lanobe-gazou").ready(
+  function(){
+    $("a img") .hover(function(){
+       $(this).fadeTo("100",0.3); // マウスオーバーで透明度を30%にする
     },function(){
-       $(this).fadeTo("4000",1.0); // マウスアウトで透明度を100%に戻す
+       $(this).fadeTo("100",1.0); // マウスアウトで透明度を100%に戻す
     });
+  });
+
 });
 
 
