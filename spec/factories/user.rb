13fab6bf50cb605f95b6f8dd26982364a45dee2b
ryoki-0703 <tacchimi504@gmail.com>
2021-03-24 12:56:11 +0000

    フォロー機能のRspecテスト完成

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,19 @@ FactoryBot.define do
     email { "test@mail" }
     password { "111111" }
     password_confirmation { "111111" }
+
+    factory :user1 do
+      name {"ユーザー1"}
+      email { "test1@mail" }
+      password { "222222" }
+      password_confirmation { "222222" }
+    end
+
+    factory :user2 do
+      name {"ユーザー2"}
+      email { "test2@mail" }
+      password { "333333" }
+      password_confirmation { "333333" }
+    end
   end
 end
--- spec/models/follow_relationship_spec.rb
+++ spec/models/follow_relationship_spec.rb
@@ -1,11 +1,13 @@
+require 'rails_helper'
+
+
 RSpec.describe FollowRelationship, "モデルに関するテスト", type: :model do
   context "フォロー、フォロワー" do
     it "ユーザーが存在すればフォローできること" do
-      user = build(:user)
-      user.save
+      user1 = create(:user1)
+      user2 = create(:user2)
 
-      follow_relationship = build(:follow_relationship)
-      follow_relationship.following_id = user.id
+      follow_relationship = build(:follow_relationship, follower_id: user1.id, following_id: user2.id)
       expect(follow_relationship).to be_valid
     end
 
@@ -14,25 +16,7 @@ RSpec.describe FollowRelationship, "モデルに関するテスト", type: :mode
       user.save
 
       follow_relationship = build(:follow_relationship)
-      follow_relationship.following_id = ""
-      expect(follow_relationship).to be_invalid
-    end
-
-    it "フォローされていれば、フォロワーが表示されること" do
-      user = build(:user)
-      user.save
-
-      follow_relationship = build(:follow_relationship)
-      follow_relationship.follower_id = user.id
-      expect(follow_relationship).to be_valid
-    end
-
-    it "フォローされていなければ、フォロワーが表示されないこと" do
-      user = build(:user)
-      user.save
-
-      follow_relationship = build(:follow_relationship)
-      follow_relationship.follower_id = ""
+      follow_relationship.following_id = nil
       expect(follow_relationship).to be_invalid
     end
 
