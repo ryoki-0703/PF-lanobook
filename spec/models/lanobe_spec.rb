ae42fb661ed693cf8b8ecc1f483f43cd1ec4f441
ryoki-0703 <tacchimi504@gmail.com>
2021-03-24 01:37:54 +0000

    lanobeモデルのRspec完成

--- spec/models/lanobe_spec.rb
+++ spec/models/lanobe_spec.rb
@@ -16,6 +16,97 @@ RSpec.describe Lanobe, "モデルに関するテスト", type: :model do
   end
 
   context "空白のバリデーションチェック" do
-    it
+    it "titleが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+      user = build(:user)
+      user.save
+      category = build(:category)
+      category.save
+
+      lanobe = build(:lanobe)
+      lanobe.title = ""
+      expect(lanobe).to be_invalid
+    end
+
+    it "introductionが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+      user = build(:user)
+      user.save
+      category = build(:category)
+      category.save
+
+      lanobe = build(:lanobe)
+      lanobe.introduction = ""
+      expect(lanobe).to be_invalid
+    end
+
+    it "category_idが空白の場合にバリデーションチェックされ空白のエラーメッセージが返ってきているか" do
+      user = build(:user)
+      user.save
+      category = build(:category)
+      category.save
+
+      lanobe = build(:lanobe)
+      lanobe.category_id = ""
+      expect(lanobe).to be_invalid
+    end
+
+    it "category_idが選択されている場合,投稿できているか" do
+      user = build(:user)
+      user.save
+      category = build(:category)
+      category.save
+
+      lanobe = build(:lanobe)
+      lanobe.category_id = "1"
+      expect(lanobe).to be_valid
+    end
+  end
+
+  context "文字数のバリデーションチェック" do
+    it "titleの文字数が21文字以上の場合バリデーションチェックされエラーメッセージが返ってきているか" do
+      user = build(:user)
+      user.save
+      category = build(:category)
+      category.save
+
+      lanobe = build(:lanobe)
+      lanobe.title = "111111111122222222233"
+      expect(lanobe).to be_invalid
+    end
+
+    it "titleの文字数が20文字以下の場合,投稿できているか" do
+      user = build(:user)
+      user.save
+      category = build(:category)
+      category.save
+
+      lanobe = build(:lanobe)
+      lanobe.title = "1"
+      expect(lanobe).to be_valid
+    end
+
+    it "introductionの文字数が301文字以上の場合バリデーションチェックされエラーメッセージが返ってきているか" do
+      user = build(:user)
+      user.save
+      category = build(:category)
+      category.save
+
+      lanobe = build(:lanobe)
+      lanobe.introduction = "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+                             111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+                             111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+                             11111111111111111111111111111111111111111111111111"
+      expect(lanobe).to be_invalid
+    end
+
+    it "introductionの文字数が300文字以下の場合,投稿できているか" do
+      user = build(:user)
+      user.save
+      category = build(:category)
+      category.save
+
+      lanobe = build(:lanobe)
+      lanobe.introduction = "1"
+      expect(lanobe).to be_valid
+    end
   end
 end
